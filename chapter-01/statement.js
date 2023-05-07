ab6531f51bb2c944e99a55a5427574d79614da9c
nostrss <jintagi@gmail.com>
2023-05-07 19:12:30 +0900

    계산단계와 포맷팅 단계 분리하기

--- chapter-01/statement.js
+++ chapter-01/statement.js
@@ -2,6 +2,10 @@ const invoices = require('./invoices.json');
 const plays = require('./plays.json');
 
 function statement(invoice, plays) {
+  return renderPlainText(invoice, plays);
+}
+
+function renderPlainText(invoice, plays) {
   let result = `청구내역 (고객명: ${invoice.customer})\n`;
   for (let perf of invoice.performances) {
     // 청구 내역을 출력한다.
