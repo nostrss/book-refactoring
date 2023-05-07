f10bdbb3adbfa739177b1bcc81040a969c9e7911
nostrss <jintagi@gmail.com>
2023-05-07 16:09:48 +0900

    적립 포인트 계산 코드 추출하기

--- chapter-01/statement.js
+++ chapter-01/statement.js
@@ -27,6 +27,14 @@ function playFor(aPerformance) {
   return plays[aPerformance.playID];
 }
 
+function volumeCreditsFor(aPerformance) {
+  let volumeCredits = 0;
+  volumeCredits += Math.max(aPerformance.audience - 30, 0);
+  if ('comedy' === playFor(aPerformance).type)
+    volumeCredits += Math.floor(aPerformance.audience / 5);
+  return volumeCredits;
+}
+
 function statement(invoice, plays) {
   let totalAmount = 0;
   let volumeCredits = 0;
@@ -39,10 +47,8 @@ function statement(invoice, plays) {
 
   for (let perf of invoice.performances) {
     // 포인트를 적립한다.
-    volumeCredits += Math.max(perf.audience - 30, 0);
-    // 희극 관객 5명마다 추가 포인트를 제공한다.
-    if ('comedy' === playFor(perf).type)
-      volumeCredits += Math.floor(perf.audience / 5);
+    volumeCredits += volumeCreditsFor(perf);
+
     // 청구 내역을 출력한다.
     result += ` ${playFor(perf).name}: ${format(amountFor(perf) / 100)} (${
       perf.audience
